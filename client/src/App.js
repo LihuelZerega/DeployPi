820e1c6f2f1d485726597e1041d75c03b0511ef8
lihuel <zeregalihuel@gmail.com>
2023-07-17 12:53:31 -0300

    nombrebd

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import CreateRecipe from "./Pages/FormPage/FormPage.jsx";
 import DetailPage from "./Pages/DatailPage/DetailPage.jsx";
 import { Route, Link, Routes } from "react-router-dom";
 import axios from "axios";
-axios.defaults.baseURL = "deploypi-production-269c.up.railway.app";
+axios.defaults.baseURL = "https://deploypi-production-269c.up.railway.app";
 
 const App = () => {
   return (
